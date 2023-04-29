9704008d537612692d884ad6f5c8c10fc7f342e2
Hmidani Abdelhamid <rub460483@gmail.com>
2023-04-28 18:31:05 -0700

    file to process exctuable file

---
+++ fork.c
@@ -0,0 +1,70 @@
+#include "shell.h"
+/**
+ * currentstatus - function that get the current status
+ * @status: integer input.
+ * Return: an integer value
+ */
+int currentstatus(int *status)
+{
+	static int actual;
+
+	if (status)
+		actual = *status;
+	return (actual);
+}
+/**
+ * _frk - function that creates process and execute
+ * @p: string input1.
+ * @l: string input2.
+ * @a: integer input1.
+ * @L: integer input2.
+ * @v: string input3.
+ * @e: integer input3.
+ * @m: string input4.
+ * @f: string input5.
+ * Return: Nothing.
+ */
+
+void _frk(char **p, char *l, int a, int L, char **v, int e, char **m, char *f)
+{
+	pid_t child_pid;
+	int ty = 0, status, exist, execute;
+
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		perror("Error");
+		exit(127);
+	}
+	if (child_pid == 0)
+	{
+		if (execve(p[0], p, m) == -1)
+		{
+			exist = access((const char *) p[0], F_OK);
+			execute = access((const char *) p[0], X_OK);
+			if (exist == 0 && execute == -1)
+			{
+				_put_err(p, L, 4, v);
+				free(f), free(l);
+				free_grid(p, a), free_grid(m, e);
+				exit(126);
+			}
+			else
+			{
+				_put_err(p, L, 3, v);
+			}
+			free(f), free(l);
+			free_grid(p, a), free_grid(m, e);
+			exit(127);
+		}
+	}
+	else
+	{
+		waitpid(child_pid, &status, 0);
+		if (WIFEXITED(status))
+		{
+			ty = WEXITSTATUS(status);
+			currentstatus(&ty);
+		}
+	}
+}
